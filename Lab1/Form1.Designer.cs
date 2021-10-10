7345dc71a034e56a419dcc54d92761940647e05c
3a9lu <eduard.grinvald@yandex.ru>
2021-10-10 17:18:07 +0700

    Всё готово.

---
+++ Lab1/Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿
+namespace Lab1
+{
+    partial class Метод
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Метод));
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.рассчитатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.очиститьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(12, 194);
+            this.chart1.Name = "chart1";
+            this.chart1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            series1.BorderColor = System.Drawing.Color.Black;
+            series1.BorderWidth = 4;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.LabelForeColor = System.Drawing.Color.Transparent;
+            series1.MarkerBorderColor = System.Drawing.Color.White;
+            series1.Name = "График";
+            series2.BorderColor = System.Drawing.Color.Black;
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Color = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            series2.Name = "Точка";
+            series3.BorderColor = System.Drawing.Color.Black;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series3.Color = System.Drawing.Color.Orange;
+            series3.Name = "Шаги";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(522, 329);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            title1.Name = "График функции";
+            this.chart1.Titles.Add(title1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(145, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "-5";
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(205, 56);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(145, 22);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "5";
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(389, 56);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(145, 22);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "0,0001";
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.рассчитатьToolStripMenuItem,
+            this.очиститьToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1163, 28);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // рассчитатьToolStripMenuItem
+            // 
+            this.рассчитатьToolStripMenuItem.Name = "рассчитатьToolStripMenuItem";
+            this.рассчитатьToolStripMenuItem.Size = new System.Drawing.Size(98, 24);
+            this.рассчитатьToolStripMenuItem.Text = "Рассчитать";
+            this.рассчитатьToolStripMenuItem.Click += new System.EventHandler(this.рассчитатьToolStripMenuItem_Click);
+            // 
+            // очиститьToolStripMenuItem
+            // 
+            this.очиститьToolStripMenuItem.Name = "очиститьToolStripMenuItem";
+            this.очиститьToolStripMenuItem.Size = new System.Drawing.Size(87, 24);
+            this.очиститьToolStripMenuItem.Text = "Очистить";
+            this.очиститьToolStripMenuItem.Click += new System.EventHandler(this.очиститьToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(67, 24);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(29, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Введите a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(221, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Введите b";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(409, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Введите e";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(94, 131);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(440, 22);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.Text = "x*x-1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(29, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 25);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "f(x) =";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(204, 83);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(146, 41);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Формула";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(221, 182);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 25);
+            this.label6.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(45, 529);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 34);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Назад";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(332, 529);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(176, 34);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Вперёд";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(202, 165);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 17);
+            this.label7.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(242, 538);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 17);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "label8";
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(569, 56);
+            this.zedGraphControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(579, 467);
+            this.zedGraphControl1.TabIndex = 17;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
+            // Метод
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1163, 571);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Метод";
+            this.Text = "Метод золотого сечения";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem рассчитатьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem очиститьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+    }
+}
+
---
+++ Lab1/Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using org.mariuszgromada.math.mxparser;
+using ZedGraph;
+
+namespace Lab1
+{
+    public partial class Метод : Form
+    {
+        public int count; 
+        public static List<Point> steps = new List<Point>(); // Список для точек
+        public class Point // Сохраниние самих точек
+        {
+            public double x, y;
+            public Point(double X, double Y)
+            {
+                this.x = X;
+                this.y = Y;
+            }
+        }
+        public Метод()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;  // Вывод формы по центру экрана
+        }
+
+        private void очиститьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear(); // Очистка A
+            textBox2.Clear(); // Очистка B
+            textBox3.Clear(); // Очистка E
+            textBox4.Clear(); // Очистка формулы
+            chart1.Series[0].Points.Clear(); // Очистка графика
+            chart1.Series[1].Points.Clear(); // Очистка точки минимума
+            chart1.Series[2].Points.Clear(); // Очистка точек шагов
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            int length = textBox1.Text.Length;
+            if (length == 0 && ch == ',' && ch == '-')
+            {
+                e.Handled = true;
+            }
+            if (!Char.IsDigit(ch) && ch != 8 && (ch != ',' || textBox1.Text.Contains(",")) && ((ch != '-' || textBox1.Text.Contains("-")))) // Если число, BACKSPACE запятая или минус, то вводим
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            int length = textBox2.Text.Length;
+            if (length == 0 && ch == ',' && ch == '-')
+            {
+                e.Handled = true;
+            }
+            if (!Char.IsDigit(ch) && ch != 8 && (ch != ',' || textBox2.Text.Contains(",")) && ((ch != '-' || textBox2.Text.Contains("-")))) // Если число, BACKSPACE запятая или минус, то вводим
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            int length = textBox3.Text.Length;
+            if (length == 0 && ch == ',' && ch == '-')
+            {
+                e.Handled = true;
+            }
+            if (!Char.IsDigit(ch) && ch != 8 && (ch != ',' || textBox3.Text.Contains(",")) && ((ch != '-' || textBox3.Text.Contains("-")))) // Если число, BACKSPACE запятая или минус, то вводим
+            {
+                e.Handled = true;
+            }
+        }
+
+        
+
+        public async Task Picture(double borderA, double borderB, double Step) // Асинхронный метод отрисовки графика
+        {
+            await Task.Run(() => Graph(borderA, borderB, Step));
+        }
+
+        public void Graph(double borderA, double borderB, double Step) // Отрисовка графика
+        {
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            chart1.Series[2].Points.Clear();
+            Function f = new Function($"f(x)={textBox4.Text}");
+            int n = (int)Math.Ceiling((borderB - borderA) / Step) + 1;
+
+            double[] x = new double[n]; // Массив значений X 
+            double[] y = new double[n]; // Массив значений Y
+
+            for (int i = 0; i < n; ++i) // Расчитываем точки для графиков функции
+            {
+                x[i] = borderA + Step * i; // Вычисляем значение X
+                string form = $"f({x[i]})";
+                string withZam = form.Replace(",", "."); // Заменяем
+                Expression graph = new Expression(withZam, f);
+                y[i] = graph.calculate();
+            }
+            chart1.ChartAreas[0].AxisX.Minimum = borderA; // Оси графика
+            chart1.ChartAreas[0].AxisX.Maximum = borderB;
+
+            chart1.ChartAreas[0].AxisX.MajorGrid.Interval = Step; // Шаг сетки
+            chart1.Series[0].Points.DataBindXY(x, y); // Добавление значений в график
+        }
+
+
+        async Task<Double> Res(double a, double b, double e) // Асинхронный метод золотого сечения
+        {
+            var result = await Task.Run(() => GoldenSection(a, b, e));
+            return result;
+        }
+
+        double F(double x) // Возвращение функции
+        {
+            Argument x_arg = new Argument("x");
+            Expression fx = new Expression(textBox4.Text, x_arg);
+            x_arg.setArgumentValue(x);
+            return fx.calculate();
+        }
+
+        private double GoldenSection(double a, double b, double e)  // Метод Золотого сечения
+        {
+            double d = (-1 + Math.Sqrt(5)) / 2;
+            double x1, x2;
+            while (true)
+            {
+                x1 = b - (b - a) * d;
+                x2 = a + (b - a) * d;
+                if (F(x1) >= F(x2))
+                {
+                    a = x1;
+                    Point p = new Point(a, F(a));
+                    steps.Add(p);
+                }
+                else
+                {
+                    b = x2;
+                    Point p = new Point(b, F(b));
+                    steps.Add(p);
+                }
+                if (Math.Abs(b - a) < e)
+                    break;
+            }
+
+            //chart1.Series[1].Points.Clear(); // Очистка предыдущей точки
+            //chart1.Series[1].Points.AddXY(c1, f1(c1)); // Добавление точки на график
+            return (a + b) / 2;
+
+        }
+
+
+        private async void рассчитатьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "")
+            {
+                MessageBox.Show("Заполните все поля!", "Ошибка!");
+                return;
+            }
+
+            /*if (textBox3.Text.Length > 12) // 10 знаков после запятой
+            {
+                MessageBox.Show("Уменьшите Epsilon", "Ошибка!");
+                return;
+            }*/
+
+            try
+            {
+                string a, b;
+                a = Convert.ToString(textBox1.Text);
+                b = Convert.ToString(textBox2.Text);
+
+
+                if (Convert.ToDouble(a) == Convert.ToDouble(b) || Convert.ToDouble(a) > Convert.ToDouble(b)) // Проверка, больше ли b хотя бы на 1
+                {
+                    MessageBox.Show("\"b\" должно быть больше \"a\" хотя бы на 1", "Ошибка");
+                    return;
+                }
+
+
+
+                double a11 = Convert.ToDouble(a);
+                double b11 = Convert.ToDouble(b);
+                double eps = Convert.ToDouble(textBox3.Text);
+
+                double result = await Res(a11, b11, eps);
+
+                double borderA = Convert.ToDouble(textBox1.Text); // Граница A
+                double borderB = Convert.ToDouble(textBox2.Text); // Граница B
+                double Step = 0.5; // Шаг
+
+                await Picture(borderA, borderB, Step);
+                label8.Text = Convert.ToString(result); // Вывод значения
+
+                count = 0;
+
+                chart1.Series[1].Points.AddXY(result, F(result));
+
+                Point end = new Point(result, F(result));//добавляем конечную точку
+                steps.Add(end);
+
+                label8.Text = $"Шаг {count}/{steps.Count}";
+
+
+                double amin, bmax;
+                PointPairList stack1 = new PointPairList();
+                PointPairList stack2 = new PointPairList();
+                GraphPane pane = zedGraphControl1.GraphPane;
+                pane.CurveList.Clear();
+                pane.GraphObjList.Clear();
+
+                amin = Convert.ToDouble(textBox1.Text);
+                bmax = Convert.ToDouble(textBox2.Text);
+                
+                for (double x = amin; x <= bmax; x += 0.1) // Рисование графика по границам
+                {
+                    stack1.Add(x, F(x));
+                }
+
+                double minX = GoldenSection(double.Parse(textBox1.Text), double.Parse(textBox2.Text), double.Parse(textBox3.Text)); // Точка минимума
+                stack2.Add(minX, F(minX));
+
+                LineItem Curve = pane.AddCurve(textBox4.Text, stack1, Color.FromArgb(184, 57, 8), SymbolType.None);
+                LineItem Min = pane.AddCurve("Минимум", stack2, Color.Black, SymbolType.XCross);
+                
+                zedGraphControl1.AxisChange(); // Обновление графика
+
+                Argument x_arg = new Argument("x");
+                GraphPane graphfield = zedGraphControl1.GraphPane;
+                graphfield.Title.Text = "График";
+                zedGraphControl1.Invalidate();
+            }
+
+            catch
+            {
+                MessageBox.Show("Проверьте введёные значения!", "Ошибка!");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) // Кнопка назад
+        {
+            if (count >= 1)
+            {
+                chart1.Series[2].Points.Clear(); // Очищаем предыдущую
+                chart1.Series[2].Points.AddXY(steps[count - 1].x, steps[count - 1].y);
+                label8.Text = $"Шаг {count}/{steps.Count}";
+                --count;
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e) // Кнопка вперёд
+        {
+            if (count < steps.Count)
+            {
+                chart1.Series[2].Points.Clear(); // Очищаем предыдущую
+                chart1.Series[2].Points.AddXY(steps[count].x, steps[count].y);
+                ++count;
+                label8.Text = $"Шаг {count}/{steps.Count}";
+            }
+        }
+    }
+}
